656d09052051788b26b35fe5d881d344fece5236
damlazirek <138490909+damlazirek@users.noreply.github.com>
2026-05-04 18:48:49 +0300

    Update Account.java

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -1,1 +1,11 @@
+public class Account {
+    // Şifre en az 8 karakter olmalı
+    public boolean validatePassword(String password) {
+        return password != null && password.length() >= 8;
+    }
 
+    // Yaş en az 18 olmalı
+    public boolean validateAge(int age) {
+        return age >= 18;
+    }
+}
